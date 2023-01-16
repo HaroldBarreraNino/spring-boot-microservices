31e5722e76dfb19109744f3279d8ac64f01bbf3a
HaroldBarreraNino <hsbarreran@outlook.com>
2023-01-16 15:08:55 -0500

    Conexion multiple h2 attempt 1

--- backend/microservicio-operador/src/main/java/com/example/microserviciooperador/MicroservicioOperadorApplication.java
+++ backend/microservicio-operador/src/main/java/com/example/microserviciooperador/MicroservicioOperadorApplication.java
@@ -1,12 +1,22 @@
 package com.example.microserviciooperador;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioOperadorApplication {
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "8090");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicioOperadorApplication.class, args);
